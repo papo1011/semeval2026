4dce1ab095191a32ecb9ec993a0a07d0ec8e1192
papo1011 <francesco.papini.siena@gmail.com>
2025-12-11 23:19:16 +0100

    feat: add script for GPU training on hpc

---
+++ baselines/train_hpc.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --job-name=semeval
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=<ADDYOUREMAIL>
+#SBATCH --partition=gpu_a40           # choose between gpu_a40, gpu_a40_ext, gpu_a100, cpu_sapphire, cpu_sapphire_ext
+#SBATCH --time=0-05:00:00             # Max time
+#SBATCH --cpus-per-task=8             # CPUs per task
+#SBATCH --mem=64G                     # Memory required per node
+
+module purge
+module load miniforge/24.3.0-0
+module load nvhpc/25.1
+module load gcc/12.4.0
+
+eval "$(conda shell.bash hook)"
+# before running this script, make sure you have created the conda environment
+conda activate semeval_env
+
+python train.py
