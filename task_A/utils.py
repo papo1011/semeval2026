809e724de4ab7f29b5e73320479f658db9689673
EDOARDO COTTO s343449 <s343449@studenti.polito.it>
2026-04-11 12:29:47 +0200

    load dataset fixing

--- task_A/utils.py
+++ task_A/utils.py
@@ -1,11 +1,18 @@
 from datasets import load_dataset
 import pandas as pd
 
-def load_dataset_XGB():
-    dataset = load_dataset("DaniilOr/SemEval-2026-Task13")
-    data = list(dataset)
-    df = pd.DataFrame(data)
-    df_train = df[df['split'] == 'train'].reset_index(drop=True)
-    df_val = df[df['split'] == 'validation'].reset_index(drop=True)
-    df_test = df[df['split'] == 'test'].reset_index(drop=True)
+def load_dataset_XGB(config_name):
+    dataset = load_dataset("DaniilOr/SemEval-2026-Task13", config_name)
+
+    # load_dataset returns a DatasetDict for this benchmark
+    df_train = dataset["train"].to_pandas().reset_index(drop=True)
+    df_val = dataset["validation"].to_pandas().reset_index(drop=True)
+    df_test = dataset["test"].to_pandas().reset_index(drop=True)
+
+    # Print the sizes of the datasets
+    print(f"Train set size: {len(df_train)}")
+    print(f"Validation set size: {len(df_val)}")
+    print(f"Test set size: {len(df_test)}")
+
+    #return the datasets as pandas DataFrames
     return df_train, df_val, df_test
