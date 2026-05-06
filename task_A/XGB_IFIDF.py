86d623d8b9f7b56a09923e36fbfe9b7bd96a05ec
SABRINA BONFANTE s343496 <s343496@studenti.polito.it>
2026-05-06 17:55:38 +0200

    Aggiunta epoche

--- task_A/XGB_IFIDF.py
+++ task_A/XGB_IFIDF.py
@@ -15,7 +15,8 @@ def train_eval(X_train, X_test, y_train, y_test):
                         device="cuda")
     
     xgb.fit(X_train, 
-            y_train)
+            y_train,
+            epoch=10)
     
     
     y_prob = xgb.predict_proba(X_test)[:, 1]
